c578cf06751024ff312f0c519fa613e57b6b07e6
Jacques Twizeyimana <sandbergjacques500@gmail.com>
2022-02-17 15:20:44 +0200

    feat: navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ interface Props {
 const Header: React.FC<Props> = ({ title }) => {
   return (
     <header className="bg-white shadow">
-      <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
         <h1 className="text-3xl font-bold leading-tight text-gray-900">
           {title}
         </h1>
