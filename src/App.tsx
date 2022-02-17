b0ab750acab10626a46cf55470d7ddb2180459ee
Jacques Twizeyimana <sandbergjacques500@gmail.com>
2022-02-17 15:22:18 +0200

    feat: add app navigation, load meter with token

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-import Nav from './components/Nav'
 
-import Home from './views/Home'
-import About from './views/About'
-import NotFound from './views/NotFound'
+import Home from "./views/Home";
+import NotFound from "./views/NotFound";
+import Navbar from "./components/Navbar";
+import Buy from "./views/Buy";
+import Load from "./views/Load";
+import Balance from "./views/Balance";
 
 const App = () => {
   return (
     <Router>
-      <Nav />
+      <Navbar />
       <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route>
-          <NotFound />
-        </Route>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/buy" component={Buy} />
+        <Route exact path="/load" component={Load} />
+        <Route exact path="/balance" component={Balance} />
+        <Route component={NotFound} />
       </Switch>
     </Router>
   );
